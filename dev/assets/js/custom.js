5226bfa68e0e6a4e715e4ca39191e1679e788d65
unknown <olamygol14@gmail.com>
2023-06-20 12:17:54 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   
     $('.first-screen-slider').slick({
         dots:true,
+        infinity:true,
+        appendArrows: '.first-screen-slider__arrows'
     });
 
   });
