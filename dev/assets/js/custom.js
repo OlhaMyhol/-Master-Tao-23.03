c168c6c65540ae3819f34b841a109b9dc9fee564
unknown <olamygol14@gmail.com>
2023-06-21 11:16:21 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -3,8 +3,17 @@ $(document).ready(function(){
     $('.first-screen-slider').slick({
         dots:true,
         infinity:true,
-        appendArrows: '.first-screen-slider__arrows'
-    });
+        appendArrows: '.first-screen-slider__arrows',
+        responsive: [{
+            
+              breakpoint: 991,
+              settings: {
+                arrows:false,
+              }
+            },
+          ]
+        });
+});
 
-  });
+ 
 
