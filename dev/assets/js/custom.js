7c304f903a358f7d85a3cb6291dd775bc5a54e17
unknown <olamygol14@gmail.com>
2023-07-01 19:33:40 +0300

    form commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,25 +1,52 @@
-$(document).ready(function(){
-  
-    $('.first-screen-slider').slick({
-        dots:true,
-        infinity:true,
-        appendArrows: '.first-screen-slider__arrows',
-        responsive: [{
-            
+$(document).ready(function() {
+
+  $('.first-screen-slider').slick({
+      dots: true,
+      infinite: true,
+      appendArrows: '.first-screen-slider-arrows',
+      responsive: [{
               breakpoint: 991,
               settings: {
-                arrows:false,
+                  arrows: false,
               }
-            },
-          ]
-        });
+          },
 
-        
+      ]
+  });
+
+  mobileOnlySlider("#advantages-slider", true, false, 991);
+  function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+  var slider = $($slidername);
+  var settings = {
+      mobileFirst: true,
+      dots: $dots,
+      arrows: $arrows,
+      responsive: [
+      {
+          breakpoint: $breakpoint,
+          settings: "unslick"
+      }
+      ]
+  };
+
+  slider.slick(settings);
+
+  $(window).on("resize", function () {
+      if ($(window).width() > $breakpoint) {
+      return;
+      }
+      if (!slider.hasClass("slick-initialized")) {
+      return slider.slick(settings);
+      }
+  });
+  } 
 });
 
- const checkbox = document.querySelectorAll('[type=checkbox]')
 
- checkbox.forEach(item => {
-  item.closest('.input-box').classList.add ('has-checkbox')
+const checkbox = document.querySelectorAll('[type="checkbox"]')
+
+checkbox.forEach(item => {
+  item.closest('.input-box').classList.add('has-checkbox')
   item.closest('.input-column').classList.add('padding')
 })
+
